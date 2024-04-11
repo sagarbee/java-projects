6772da430b3fb9be4527ffcc6ae4140d6c565266
sagar <sagarmore1752000@gmail.com>
2024-04-11 22:55:04 +0530

    Student update
    merger is polymorphic method i.e if id don't have student with id 1 when i am trying update id 1 student record it will insert new record with id 1. to make merge only act like update i have use conditional statement to handle null id

--- hibernate_student/src/main/java/hibernate_student/StudentUpdate.java
+++ hibernate_student/src/main/java/hibernate_student/StudentUpdate.java
@@ -1,10 +1,30 @@
 package hibernate_student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class StudentUpdate {
 
 	public static void main(String[] args) {
 
-		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("sagar");
+		EntityManager manager = factory.createEntityManager();
+		EntityTransaction transaction = manager.getTransaction();
+
+		Student student = manager.find(Student.class, 1);
+		student.setMarks(99.98d);
+		student.setPhone(83089044134l);
+		if (student != null) {
+			transaction.begin();
+			manager.merge(student);
+			transaction.commit();
+
+		} else {
+			System.out.println("Student ID not found");
+		}
+
 	}
 
 }
