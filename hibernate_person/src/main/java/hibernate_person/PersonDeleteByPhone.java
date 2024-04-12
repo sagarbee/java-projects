ff56a01f9261e1e8c4b7601790d9e3c7a9a29f35
sagar <sagarmore1752000@gmail.com>
2024-04-12 23:24:54 +0530

    Handle NoResultException of getSingleResult()

--- hibernate_person/src/main/java/hibernate_person/PersonDeleteByPhone.java
+++ hibernate_person/src/main/java/hibernate_person/PersonDeleteByPhone.java
@@ -3,6 +3,7 @@ package hibernate_person;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
@@ -13,17 +14,19 @@ public class PersonDeleteByPhone {
 		EntityManager manager = factory.createEntityManager();
 		EntityTransaction transaction = manager.getTransaction();
 
-		long phone = 8994587895L;
+		long phone = 89945895L;
 		Query query = manager.createQuery("select p from Person p where p.phone =: phone");
 		query.setParameter("phone", phone);
-		Person person = (Person) query.getSingleResult();
-		
-		if (person != null) {
-			transaction.begin();
-			manager.remove(person);
-			transaction.commit();
-		} else {
-			System.out.println("Student does not found");
+
+		try {
+			Person person = (Person) query.getSingleResult();
+			if (person != null) {
+				transaction.begin();
+				manager.remove(person);
+				transaction.commit();
+			}
+		} catch (NoResultException e) {
+			System.out.println("Person does not found");
 		}
 
 	}
