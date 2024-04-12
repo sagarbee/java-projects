489029ed29bb67e754d2d733531bec36c704f676
sagar <sagarmore1752000@gmail.com>
2024-04-12 23:26:39 +0530

    Person delete by name

--- hibernate_person/src/main/java/hibernate_person/PersonDeleteByName.java
+++ hibernate_person/src/main/java/hibernate_person/PersonDeleteByName.java
@@ -3,30 +3,29 @@ package hibernate_person;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-public class PersonDeleteById {
-
+public class PersonDeleteByName {
 	public static void main(String[] args) {
-		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("sagar");
 		EntityManager manager = factory.createEntityManager();
 		EntityTransaction transaction = manager.getTransaction();
-		
-		Person person = manager.find(Person.class, 4);
-		
-		if(person!=null)
-		{
-			transaction.begin();
-			manager.remove(person);
-			transaction.commit();
-		}
-		else
-		{
+
+		String name = "Ruvighna";
+		Query query = manager.createQuery("select p from Person p where p.name =: name");
+		query.setParameter("name", name);
+
+		try {
+			Person person = (Person) query.getSingleResult();
+			if (person != null) {
+				transaction.begin();
+				manager.remove(person);
+				transaction.commit();
+			}
+		} catch (NoResultException e) {
 			System.out.println("Person does not found");
 		}
-		
 	}
-
 }
